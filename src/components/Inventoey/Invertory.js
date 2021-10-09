74b1b13dbfeacb375b79b7c7334fdf362fc566f7
JOY <joy.contact.21@gmail.comm>
2021-10-09 15:14:50 +0600

    inventory added

---
+++ src/components/Inventoey/Invertory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Invertory = () => {
+    return (
+        <div>
+            <h2>This is Github repogetory</h2>
+            <p>new Order received</p>
+        </div>
+    );
+};
+
+export default Invertory;
